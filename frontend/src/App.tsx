cab5c1cff671c046be0ea37373a55eaa1d583667
nehajha25 <nehajha1925@gmail.com>
2026-02-11 12:01:55 +0530

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>Mern Project</header>
+      <main></main>
+      <footer>thank you</footer>
+    </>
+  )
+}
+
+export default App
